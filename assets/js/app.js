5f9f6820391e4fc0cdbd081cc9447f863c97e2e2
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-11-13 00:30:35 -0500

    first version ecommerce made

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  console.clear();
+  console.log("document ready");
+
+  let cartCount = document.querySelector('.fa-shopping-bag')
+  let countProduct = 0;
+
+  for (let i = 1; i < 10; i++) {
+    let imageTemplate = document.querySelector(`img[src="./assets/img/${i}.webp"]`)
+    let imageOriginal = `./assets/img/${i}.webp`;
+    let image = `./assets/img/${i}-hover.webp`
+    let btn = document.querySelector(`.btn-shop${i}`)
+
+    imageTemplate.addEventListener('mouseover', () => {
+      imageTemplate.src = image
+    })
+
+    imageTemplate.addEventListener('mouseleave', () => {
+      imageTemplate.src = imageOriginal
+    })
+
+    btn.addEventListener('click', () => {
+      countProduct += 1
+      cartCount.setAttribute("count", countProduct)
+      console.log(cartCount);
+    })
+  }
+
+
+});
