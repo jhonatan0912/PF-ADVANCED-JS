eb79b2f5f1c45021e342907598b1e305f91a33ab
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-11-14 01:12:22 -0500

    improve file app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,26 +2,25 @@ $(document).ready(function () {
   console.clear();
   console.log("document ready");
 
-  let cartCount = document.querySelector('.fa-shopping-bag')
+  let cartCount = document.querySelector('.fa-shopping-bag');
   let countProduct = 0;
 
   for (let i = 1; i < 10; i++) {
-    let imageTemplate = document.querySelector(`img[src="./assets/img/${i}.webp"]`)
+    let imageTemplate = document.querySelector(`img[src="./assets/img/${i}.webp"]`);
     let imageOriginal = `./assets/img/${i}.webp`;
-    let image = `./assets/img/${i}-hover.webp`
-    let btn = document.querySelector(`.btn-shop${i}`)
+    let image = `./assets/img/${i}-hover.webp`;
+    let btn = document.querySelector(`.btn-shop${i}`);
 
-    imageTemplate.addEventListener('mouseover', () => { imageTemplate.src = image })
+    imageTemplate.addEventListener('mouseover', () => { imageTemplate.src = image });
 
-    imageTemplate.addEventListener('mouseleave', () => { imageTemplate.src = imageOriginal })
+    imageTemplate.addEventListener('mouseleave', () => { imageTemplate.src = imageOriginal });
 
     btn.addEventListener('click', () => {
-      countProduct += 1
-      cartCount.setAttribute("count", countProduct)
-      console.log(cartCount);
+      countProduct += 1;
+      cartCount.setAttribute("count", countProduct);
     })
   }
 
-  let pasarela = document.querySelector('.pasarela-container')
-  cartCount.addEventListener('click', () => { pasarela.classList.toggle('hidden') })
+  let pasarela = document.querySelector('.pasarela-container');
+  cartCount.addEventListener('click', () => { pasarela.classList.toggle('hidden') });
 });
